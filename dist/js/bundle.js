0e6741a708ea329309a8e68de4c250366c41ece7
Mariya <marikor168@gmail.com>
2019-05-26 19:06:33 +0300

    Form

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -229,18 +229,108 @@ module.exports = feedbackSlider;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-function formDesign() {
+var formDesign = function formDesign() {
+  var message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+  };
   var form = document.querySelector('.popup-design form'),
       input = form.getElementsByTagName('input'),
       inputName = input[1],
       inputPhone = input[2],
       inputMail = input[3],
-      comment = form.getElementsByTagName('textarea')[0];
+      comment = form.getElementsByTagName('textarea')[0],
+      statusMessage = document.createElement('div');
   console.log(inputName);
   console.log(inputPhone);
   console.log(inputMail);
-  console.log(comment);
-}
+  console.log(comment); // inputPhone.addEventListener('input', () => {
+  //     inputPhone.value = inputPhone.value.replace(/[^0-9+]/g, "");
+  //     inputPhone.value = inputPhone.value.replace(/(?<!^)\+/g, "");
+  // });
+  // inputName.addEventListener('input', () => {
+  //     inputName.value = inputName.value.replace(/[^А-я]/g, "");
+  // });
+  // comment.addEventListener('input', () => {
+  //     comment.value = comment.value.replace(/[^А-я]/g, "");
+  // });
+  // let telPlaceholder = "+7(___) ___-____";
+  // let numsOnly = "";
+  // inputPhone.addEventListener('focus', () => {
+  //     if (numsOnly.length !== 10) {
+  //         inputPhone.value = telPlaceholder;
+  //     }
+  // });
+  // inputPhone.addEventListener('input', () => {
+  //     let rawTel = inputPhone.value;
+  //     numsOnly = rawTel.replace(/\D/g, "");
+  //     let result = telPlaceholder.split("");
+  //     for (let i = 0, j = 0; i < result.length; i++) {
+  //         // if (i == 0 || i == 4 || i == 5 || i == 9){
+  //         if (i == 0 || i == 1 || i == 2 || i == 6 || i == 7 || i == 11) {
+  //             continue;
+  //         }
+  //         let tmp = numsOnly[j++];
+  //         if (tmp !== undefined) {
+  //             result[i] = tmp;
+  //         }
+  //     }
+  //     inputPhone.value = result.join("");
+  //     if (numsOnly.length > 10) {
+  //         numsOnly = numsOnly.substr(0, 10);
+  //     }
+  //     setCursorPosition();
+  // });
+  // function setCursorPosition() {
+  //     let length = numsOnly.length;
+  //     if (length >= 2 && length <= 5) {
+  //         inputPhone.setSelectionRange(length + 3, length + 3);
+  //     } else if (length >= 6 && length <= 8) {
+  //         inputPhone.setSelectionRange(length + 5, length + 5);
+  //     } else {
+  //         inputPhone.setSelectionRange(length + 6, length + 6);
+  //     }
+  // }
+  // inputPhone.addEventListener('blur', () => {
+  //     if (numsOnly.length !== 10) {
+  //         inputPhone.value = '';
+  //     }
+  // });
+
+  var sendForm = function sendForm(elem) {
+    elem.addEventListener('submit', function (event) {
+      event.preventDefault();
+      elem.appendChild(statusMessage);
+      var formData = new FormData(elem);
+
+      var postData = function postData(data) {
+        return fetch('server.php', {
+          method: 'POST',
+          body: data
+        });
+      };
+
+      var clearInput = function clearInput() {
+        for (var i = 0; i < input.length; i++) {
+          input[i].value = '';
+        }
+
+        comment.value = '';
+      };
+
+      postData(formData).then(function () {
+        return statusMessage.innerHTML = message.loading;
+      }).then(function () {
+        statusMessage.innerHTML = message.success;
+      }).catch(function () {
+        return statusMessage.innerHTML = message.failure;
+      }).then(clearInput);
+    });
+  };
+
+  sendForm(form);
+};
 
 module.exports = formDesign;
 
@@ -315,8 +405,20 @@ var modals = function modals() {
   modalConsult.classList.add('fadeIn');
   gift.classList.add('animated');
   modalGift.classList.add('animated');
-  modalGift.classList.add('fadeIn');
+  modalGift.classList.add('fadeIn'); // console.log(body.getBoundingClientRect());
+  // console.log(Math.abs(body.getBoundingClientRect().y) + body.getBoundingClientRect().bottom + 1);
+  // console.log(body.clientHeight);
+
+  console.log(body.scrollTop);
+  window.addEventListener('scroll', function () {
+    console.log(window.pageYOffset);
+    console.log(document.documentElement.scrollTop);
+    console.log(Math.abs(body.getBoundingClientRect().y));
+  });
+  var clickBtn = false;
   body.addEventListener('click', function (event) {
+    clickBtn = true;
+
     if (event.target.classList.contains('button-design')) {
       modalOpen(modalDesign);
     }
@@ -341,6 +443,12 @@ var modals = function modals() {
     if (event.target.classList.contains('popup-gift') || event.target.classList.contains('popup-close')) {
       modalClose(modalGift);
       gift.style.display = 'block';
+    }
+  });
+  window.addEventListener('scroll', function () {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !clickBtn) {
+      modalOpen(modalGift);
+      gift.style.display = 'none';
     }
   });
 };
--- src/js/parts/modal.js
+++ src/js/parts/modal.js
@@ -24,7 +24,21 @@ let modals = () => {
     modalGift.classList.add('animated');
     modalGift.classList.add('fadeIn');
 
+    // console.log(body.getBoundingClientRect());
+    // console.log(Math.abs(body.getBoundingClientRect().y) + body.getBoundingClientRect().bottom + 1);
+    // console.log(body.clientHeight);
+    console.log(body.scrollTop);
+    window.addEventListener('scroll', () => {
+
+        console.log(window.pageYOffset);
+        console.log(document.documentElement.scrollTop);
+        console.log(Math.abs(body.getBoundingClientRect().y));
+    });
+
+    let clickBtn = false;
+
     body.addEventListener('click', (event) => {
+        clickBtn = true;
         if (event.target.classList.contains('button-design')) {
             modalOpen(modalDesign);
         }
@@ -44,6 +58,15 @@ let modals = () => {
         if (event.target.classList.contains('popup-gift') || event.target.classList.contains('popup-close')) {
             modalClose(modalGift);
             gift.style.display = 'block';
+        }
+        
+    });
+
+    window.addEventListener('scroll', () => {
+
+        if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && !clickBtn) {
+            modalOpen(modalGift);
+            gift.style.display = 'none'; 
         }
     });
 };
