5db1656f604afa11b33fc3134602f7944453f5b8
Mariya <marikor168@gmail.com>
2019-05-28 14:22:57 +0300

    Calc

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -218,7 +218,8 @@ var calc = function calc() {
       totalValue = document.querySelector('.calc-price'),
       total = 1000,
       indexSize = 0,
-      indexMaterial = 0;
+      indexMaterial = 0,
+      promo = false;
   size.options[0].value = 0;
   size.options[1].value = 0.5;
   size.options[2].value = 1;
@@ -227,36 +228,17 @@ var calc = function calc() {
   material.options[0].value = 0;
   material.options[1].value = 0.5;
   material.options[2].value = 1;
-  material.options[3].value = 1.5;
+  material.options[3].value = 1.5; // services.options[0].value = 0;
+  // services.options[1].value = 1.5;
+  // services.options[2].value = 2;
+  // services.options[3].value = 2.5;
+
   services.options[0].value = 0;
   services.options[1].value = 1.5;
   services.options[2].value = 2;
-  services.options[3].value = 2.5; // console.log(size);
-  // console.log(material);
-  // console.log(promocode);
-
+  services.options[3].value = 2.5;
   size.addEventListener('change', function () {
     indexSize = +size.options[size.selectedIndex].value;
-    total = 1000; // console.log('total', total);
-    // console.log('indexSize', size.options[size.selectedIndex].value);
-    // console.log('indexMaterial', material.options[material.selectedIndex].value);
-    // console.log('indexSize + indexMaterial',indexSize + indexMaterial);
-    // console.log('total',total);
-
-    if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
-      totalValue.innerHTML = 0;
-    } else {
-      total = (indexSize + indexMaterial) * total;
-      totalValue.innerHTML = total + 1000 * +services.options[services.selectedIndex].value;
-    }
-  });
-  material.addEventListener('change', function () {
-    indexMaterial = +material.options[material.selectedIndex].value; // console.log('total', total);
-    // console.log('indexSize', size.options[size.selectedIndex].value);
-    // console.log('indexMaterial', material.options[material.selectedIndex].value);
-    // console.log('indexSize + indexMaterial',indexSize + indexMaterial);
-    // console.log('total',total);
-
     total = 1000;
 
     if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
@@ -264,6 +246,19 @@ var calc = function calc() {
     } else {
       total = (indexSize + indexMaterial) * total;
       totalValue.innerHTML = total + 1000 * +services.options[services.selectedIndex].value;
+      promocode.value = '';
+    }
+  });
+  material.addEventListener('change', function () {
+    indexMaterial = +material.options[material.selectedIndex].value;
+    total = 1000;
+
+    if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
+      totalValue.innerHTML = 0;
+    } else {
+      total = (indexSize + indexMaterial) * total;
+      totalValue.innerHTML = total + 1000 * +services.options[services.selectedIndex].value;
+      promocode.value = '';
     }
   });
   services.addEventListener('change', function () {
@@ -272,37 +267,21 @@ var calc = function calc() {
     if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
       totalValue.innerHTML = 0;
     } else {
-      // console.log(total);
       var a = 1000;
       total = (indexSize + indexMaterial) * total + a * +this.options[this.selectedIndex].value;
-      totalValue.innerHTML = total; // console.log(total);
+      totalValue.innerHTML = total;
+      promocode.value = '';
     }
   });
   promocode.addEventListener('input', function () {
-    document.addEventListener('click', function (event) {
-      var isClickInside = promocode.contains(event.target);
-
-      if (!isClickInside) {
-        //the click was outside the specifiedElement, do something
-        if (promocode.value === 'IWANTPOPART') {
-          console.log('true');
-          console.log(total);
-          totalValue.innerHTML = total - total * 0.3; // totalValue.innerHTML = (total + total * +services.options[services.selectedIndex].value) - (total + total * +services.options[services.selectedIndex].value) * 0.3;
-        }
-      }
-    });
-  }); // promocode.addEventListener('input', () => {
-  //     console.log(total);
-  //     if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
-  //         totalValue.innerHTML = 0;
-  //     } 
-  //     if (promocode.value === 'IWANTPOPART') {
-  //         console.log('true');
-  //         console.log(total);
-  //         totalValue.innerHTML = total - total * 0.3;
-  //         // totalValue.innerHTML = (total + total * +services.options[services.selectedIndex].value) - (total + total * +services.options[services.selectedIndex].value) * 0.3;
-  //     }
-  // });
+    if (promocode.value === 'IWANTPOPART') {
+      promo = true;
+      totalValue.innerHTML = total - total * 0.3;
+    } else {
+      totalValue.innerHTML = total;
+      promo = false;
+    }
+  });
 };
 
 module.exports = calc;
--- src/js/parts/calc.js
+++ src/js/parts/calc.js
@@ -6,7 +6,8 @@ let calc = () => {
         totalValue = document.querySelector('.calc-price'),
         total = 1000,
         indexSize = 0,
-        indexMaterial = 0;
+        indexMaterial = 0,
+        promo = false;
 
     size.options[0].value = 0;
     size.options[1].value = 0.5;
@@ -19,14 +20,15 @@ let calc = () => {
     material.options[2].value = 1;
     material.options[3].value = 1.5;
 
+    // services.options[0].value = 0;
+    // services.options[1].value = 1.5;
+    // services.options[2].value = 2;
+    // services.options[3].value = 2.5;
+
     services.options[0].value = 0;
     services.options[1].value = 1.5;
     services.options[2].value = 2;
     services.options[3].value = 2.5;
-    
-    // console.log(size);
-    // console.log(material);
-    // console.log(promocode);
 
     size.addEventListener('change', function () {
 
@@ -34,29 +36,18 @@ let calc = () => {
 
         total = 1000;
 
-        // console.log('total', total);
-        // console.log('indexSize', size.options[size.selectedIndex].value);
-        // console.log('indexMaterial', material.options[material.selectedIndex].value);
-        // console.log('indexSize + indexMaterial',indexSize + indexMaterial);
-        // console.log('total',total);
-
         if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
             totalValue.innerHTML = 0;
         } else {
             total = (indexSize + indexMaterial) * total;
             totalValue.innerHTML = total + 1000 * +services.options[services.selectedIndex].value;
+            promocode.value = '';
         }
     });
 
     material.addEventListener('change', function () {
         indexMaterial = +material.options[material.selectedIndex].value;
 
-        // console.log('total', total);
-        // console.log('indexSize', size.options[size.selectedIndex].value);
-        // console.log('indexMaterial', material.options[material.selectedIndex].value);
-        // console.log('indexSize + indexMaterial',indexSize + indexMaterial);
-        // console.log('total',total);
-
         total = 1000;
 
         if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
@@ -64,55 +55,33 @@ let calc = () => {
         } else {
             total = (indexSize + indexMaterial) * total;
             totalValue.innerHTML = total + 1000 * +services.options[services.selectedIndex].value;
+            promocode.value = '';
         }
     });
-    
+
     services.addEventListener('change', function () {
         total = 1000;
         if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
             totalValue.innerHTML = 0;
         } else {
-
-            // console.log(total);
             let a = 1000;
             total = (indexSize + indexMaterial) * total + a * +this.options[this.selectedIndex].value;
             totalValue.innerHTML = total;
-            // console.log(total);
-        
+            promocode.value = '';
         }
     });
 
-    
     promocode.addEventListener('input', () => {
-        document.addEventListener('click', function(event) {
-            var isClickInside = promocode.contains(event.target);
-        
-            if (!isClickInside) {
-                //the click was outside the specifiedElement, do something
-                if (promocode.value === 'IWANTPOPART') {
-                    console.log('true');
-                    console.log(total);
-        
-                    totalValue.innerHTML = total - total * 0.3;
-                    // totalValue.innerHTML = (total + total * +services.options[services.selectedIndex].value) - (total + total * +services.options[services.selectedIndex].value) * 0.3;
-                }
-            }
-        });
+
+        if (promocode.value === 'IWANTPOPART') {
+            promo = true;
+            totalValue.innerHTML = total - total * 0.3;
+        } else {
+            totalValue.innerHTML = total;
+            promo = false;
+        }
+
     });
-    // promocode.addEventListener('input', () => {
-    //     console.log(total);
-
-    //     if (size.options[size.selectedIndex].value == 0 || material.options[material.selectedIndex].value == 0) {
-    //         totalValue.innerHTML = 0;
-    //     } 
-    //     if (promocode.value === 'IWANTPOPART') {
-    //         console.log('true');
-    //         console.log(total);
-
-    //         totalValue.innerHTML = total - total * 0.3;
-    //         // totalValue.innerHTML = (total + total * +services.options[services.selectedIndex].value) - (total + total * +services.options[services.selectedIndex].value) * 0.3;
-    //     }
-    // });
 };
 
 module.exports = calc;
